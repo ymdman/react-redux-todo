026a1876f347471103b2df6c20a0875b474e2dda
ymdman <yamadakazuhiro@yamadakazuhironoMacBook-Pro.local>
2017-07-17 21:57:28 +0900

    :sparkles: フォームからtodoを追加

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from '../actions/action';
+
+let AddTodo = ({ dispatch }) => {
+  let input;
+
+  return (
+    <div>
+      <input ref={(node) => { input = node; }} />
+      <button onClick={() => { dispatch(addTodo(input.value)); input.value = ''; }}>
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
+AddTodo = connect()(AddTodo);
+
+export default AddTodo;
